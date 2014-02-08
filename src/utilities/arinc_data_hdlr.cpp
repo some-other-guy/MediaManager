c85f92571cf741f0abf32154c25c4d4b99b684d1
some-other-guy <james.maroulis@live.com>
2014-02-08 16:47:21 -0500

    Removed unrelated files

--- src/utilities/arinc_data_hdlr.cpp
+++
@@ -1,7 +0,0 @@
-#include "utilities/arinc_data_hdlr.h"
-
-ArincDataHdlr::~ArincDataHdlr(){}
-ArincDataHdlr::ArincDataHdlr( arinc::packet* data )
-{
-
-}
--- src/utilities/arinc_data_hdlr_factory.cpp
+++
@@ -1,7 +0,0 @@
-#include "utilities/arinc_data_hdlr.h"
-
-ArincDataHdlr* ArincDataHdlrFactory::make_hdlr( arinc::packet* data )
-{
-    ArincDataHdlr* hdlr = 0;
-    return hdlr;
-}
